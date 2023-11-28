6a9e5ef087c2ffb5ff249b0f3b8d25e2f1688926
Ryutaro Kanno <rkanno1014@yahoo.co.jp>
2023-11-28 09:38:09 +0900

    Support integer only calculation

--- test.bash
+++ test.bash
@@ -1,5 +1,25 @@
- #!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2022 kanno ryutaro
+# SPDX-License-Identifier: BSD-3-Clause
 
-  out=$(seq 5 | ./plus)
+ng () {
+      echo NG at Line $1
+      res=1
+}
 
-  [ "${out}" = 15 ]
+res=0
+  ### I/O ###
+  out=$(seq 5 | ./pluss)
+  [ "${out}" = 15 ] || ng ${LINENO}
+
+  ### STRANGE INPUT ###
+  out=$(echo あ | ./pluss)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+
+  out=$(echo | ./pluss)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+  exit $res
